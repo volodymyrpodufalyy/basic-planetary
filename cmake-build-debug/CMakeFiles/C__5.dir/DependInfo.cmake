002b051e01ea6e401767231be37ac204cca48073
volodymyrpodufalyy <vpodufalyy13@gmail.com>
2020-11-30 20:41:24 +0200

    oh Veres

---
+++ cmake-build-debug/CMakeFiles/C__5.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Asus/CLionProjects/C++5/Planet.cpp" "C:/Users/Asus/CLionProjects/C++5/cmake-build-debug/CMakeFiles/C__5.dir/Planet.cpp.obj"
+  "C:/Users/Asus/CLionProjects/C++5/Planetary.cpp" "C:/Users/Asus/CLionProjects/C++5/cmake-build-debug/CMakeFiles/C__5.dir/Planetary.cpp.obj"
+  "C:/Users/Asus/CLionProjects/C++5/main.cpp" "C:/Users/Asus/CLionProjects/C++5/cmake-build-debug/CMakeFiles/C__5.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/C__5.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__5.dir/Planet.cpp.obj"
+  "CMakeFiles/C__5.dir/Planetary.cpp.obj"
+  "CMakeFiles/C__5.dir/main.cpp.obj"
+  "C__5.exe"
+  "C__5.exe.manifest"
+  "C__5.pdb"
+  "libC__5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
